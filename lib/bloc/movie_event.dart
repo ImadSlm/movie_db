adba58adf2c872119ee065c137f4ff152dcd4e2d
Imad Saleem <105483840+ImadSlm@users.noreply.github.com>
2024-10-22 15:47:43 +0200

    Add MovieEvent and MovieState files

---
+++ lib/bloc/movie_event.dart
@@ -0,0 +1,4 @@
+abstract class MovieEvent {}
+
+class FecthUpcomingMovies extends MovieEvent {}
+class SearchMovies extends MovieEvent {}
---
+++ lib/bloc/movie_state.dart
@@ -0,0 +1,9 @@
+abstract class MovieState {}
+
+class MovieLoading extends MovieState {}
+
+class MovieLoaded extends MovieState {}
+
+class MovieError extends MovieState {}
+
+//TODO : Bloc file
